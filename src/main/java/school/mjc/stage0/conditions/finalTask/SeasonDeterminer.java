80a8cf984a2ec45518550b57632e9c600136e07a
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-09 05:09:46 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,33 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        String season;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                season = "Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Spring";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Autumn";
+                break;
+            default:
+                season = "Wrong month number";
+        }
+
+        System.out.println(season);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        boolean isValid = (firstSide + secondSide > thirdSide) &&
+                (firstSide + thirdSide > secondSide) &&
+                (secondSide + thirdSide > firstSide);
 
+        System.out.println(isValid ? "this is a valid triangle" : "it's not a triangle");
     }
 }
